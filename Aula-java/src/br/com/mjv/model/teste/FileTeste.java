b54494dba1b2a92bd5ac4d4717f36fbf4b8707dd
Bruno Alessandro Mieli <bruno.a.mieli@icloud.com>
2022-04-15 02:50:02 -0300

    Update

--- Aula-java/src/br/com/mjv/model/teste/FileTeste.java
+++ Aula-java/src/br/com/mjv/model/teste/FileTeste.java
@@ -2,13 +2,6 @@ package br.com.mjv.model.teste;
 
 import java.io.File;
 
-//A classe File é a representação Java de um nome de caminho de arquivo ou diretório. 
-//Como os nomes de arquivos e diretórios têm formatos diferentes em plataformas diferentes, 
-//uma string simples não é adequada para nomeá-los. 
-//A classe File contém vários métodos para trabalhar com o nome do caminho, 
-//excluir e renomear arquivos, criar novos diretórios, listar o conteúdo de um diretório e 
-//determinar vários atributos comuns de arquivos e diretórios.
-
 public class FileTeste {
 
 	public static void main(String[] args) {
